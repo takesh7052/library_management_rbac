1dc55aa651228d635a8ea0c74419aa40e25f76f6
takesh7052 <68686658+takesh7052@users.noreply.github.com>
2021-01-21 22:00:28 +0530

    updated
    new update

--- userproject/urls.py
+++ userproject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Central Library Admin"
+admin.site.site_title = "Central Library Admin Portal"
+admin.site.index_title = "Welcome to Central Library"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
